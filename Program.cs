bc1ac6a0a5d4daa9c0a8339e5001408903a68dc1
hugo <hugofeige@hotmail.fr>
2020-10-16 10:41:52 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClubFromage_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
